e5d5bd16a8bd213242349ccacb6b331c0688ef81
bianca <rosenblut94@yahoo.de>
2021-10-25 16:28:03 +0200

    change done
    change is done

--- main.java
+++ main.java
@@ -1,11 +1,14 @@
+package myawesomeclass;
+
 public class myAwesomeClass {
 
-  public static  main(String args) {
-    int myVariable = 15;
-    for (int j = 1; j < 20; i++) {
-      myVariable = myVariable * j;
-      System.out.print(j)
-      System.out.println(" ");
-      System.outprintln(myVariable);
-    }
-}
+	  public static void main(String[] args) {
+	    int myVariable = 15;
+	    for (int j = 1; j < 20; j++) {
+	      myVariable = myVariable * j;
+	      System.out.print(j);
+	      System.out.println(" ");
+	      System.out.println(myVariable);
+	    }
+	  }
+	}
