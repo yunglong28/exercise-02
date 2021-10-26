14a7dc5ca864dc7239b42252754d6aee85d19488
Marvin Kamphausen <marvin.kamphausen@icloud.com>
2021-10-26 14:44:47 +0200

    bugfix

---
+++ myAwesomeClass.java
@@ -0,0 +1,12 @@
+public class myAwesomeClass {
+
+  public static void main(String [] args) {
+    long myVariable = 15;
+    for (int j = 1; j < 20; j++) {
+      myVariable = myVariable * j;
+      System.out.print(j);
+      System.out.print(" ");
+      System.out.println(myVariable);
+    }
+}
+}
