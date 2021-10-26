dba4dd5f7dc2b4902d92208629d64bc2e4e9a89a
BMusolff <92873072+BMusolff@users.noreply.github.com>
2021-10-26 20:19:24 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,12 @@
 public class myAwesomeClass {
 
-  public static  main(String args) {
+  public static void main(String[] args) {
     int myVariable = 15;
-    for (int j = 1; j < 20; i++) {
+    for (int j = 1; j < 20; j++) {
       myVariable = myVariable * j;
-      System.out.print(j)
+      System.out.print(j);
       System.out.println(" ");
-      System.outprintln(myVariable);
+      System.out.println(myVariable);
     }
+  }
 }
