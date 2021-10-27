774c127c112e7dac0d91e38b3e06e6871c690808
Majus317 <92587997+Majus317@users.noreply.github.com>
2021-10-27 14:36:26 +0200

    Update main.java
    Fixed the Java Beginner mistakes, the output should be correct now.

--- main.java
+++ main.java
@@ -1,11 +1,12 @@
-public class myAwesomeClass {
+public class main {
 
-  public static  main(String args) {
+  public static  void main(String[] args) {
     int myVariable = 15;
-    for (int j = 1; j < 20; i++) {
-      myVariable = myVariable * j;
-      System.out.print(j)
+    for (int i = 1; i < 20; i++) {
+      myVariable = myVariable * i;
+      System.out.println(i);
       System.out.println(" ");
-      System.outprintln(myVariable);
+      System.out.println(myVariable);
     }
+}
 }
