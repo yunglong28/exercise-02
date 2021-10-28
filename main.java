ec8a98e7eef758ade0e13cb3ca397caebd6d267f
Alessandro Y. Longo <72975038+yunglong28@users.noreply.github.com>
2021-10-28 12:54:11 +0200

    Fixed Java file
    It should work like this

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
-public class myAwesomeClass {
-
-  public static  main(String args) {
+public class myClass {
+  public static void main(String[]args) {
     int myVariable = 15;
-    for (int j = 1; j < 20; i++) {
+    for (int j = 1; j < 20; j++) {
       myVariable = myVariable * j;
-      System.out.print(j)
+      System.out.print(j);
       System.out.println(" ");
-      System.outprintln(myVariable);
+      System.out.println(myVariable);
     }
+  }
 }
